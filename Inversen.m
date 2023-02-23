68a68a849700edc933265baea7bcb88a5cbc8062
Hanokos <119046964+Hanokos@users.noreply.github.com>
2023-02-23 11:27:41 +0100

    Update Inversen.m

--- Inversen.m
+++ Inversen.m
@@ -2,30 +2,37 @@
 clear
 close all
 
-% Vektorn u
+%% Vektorn u
 u = [3; 4];
 
-% Transformera u genom rotation
+%% Transformera u genom rotation
 theta = 30; %angle in degrees
-rotering = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-roterad_u = rotering * u; %  Vekor v = Vektor u  roterad 30 grader moturs
+rotering = [cosd(theta) -sind(theta);
+            sind(theta) cosd(theta)];
+v = rotering * u; %  Vekor v = Vektor u  roterad 30 grader moturs
 
-% Transformera v genom rotation Invers
-inv_rotering = inv(rotering); % Inversen av Vektorn v
-orginal_vektor = inv_rotering * roterad_u; % Vekor w = Vektor v  roterad 30 grader medurs
+%% Transformera v genom rotation Invers
+inv_v = inv(rotering); % Inversen av Vektorn v
+w = inv_v * v; % Vekor w = Vektor v  roterad 30 grader medurs
 
-% Vektor u, Vektor v
+
+vinkel = acosd((v'*w)/(norm(v)*norm(w)));% Räknar ut vinkel mellan två vektorer. För bevisa att det stämmer
+%% plot Vektor u, Vektor v
 figure(1)
 hold on;
 plot([0 u(1)], [0 u(2)], 'b-'); 
-plot([0 roterad_u(1)], [0 roterad_u(2)], 'r-');
+plot([0 v(1)], [0 v(2)], 'r-');
 axis equal;
-legend('Vektor u', ' Vekor v = Vektor u  roterad 30 grader moturs')
+title('Vektor u, Vektor v')
+subtitle(['Grader mellan vinklarna = ', num2str(theta)])
+legend('Vektor u', ' Vekor v')
 
-% Vektor v, Vektor w
+%% plot Vektor v, Vektor w
 figure(2)
 hold on;
-plot([0 roterad_u(1)], [0 roterad_u(2)], 'r-');
-plot([0 orginal_vektor(1)], [0 orginal_vektor(2)], 'g-');
+plot([0 v(1)], [0 v(2)], 'r-');
+plot([0 w(1)], [0 w(2)], 'g-');
 axis equal;
-legend('Vektor v  ', 'Vekor w =  Vektor u = Vektor v  roterad 30 grader medurs')
+title('Vektor v, Vektor w')
+subtitle(['Grader mellan vinklarna = ', num2str(vinkel)])
+legend('Vektor v  ', 'Vekor w =  Vektor u')
