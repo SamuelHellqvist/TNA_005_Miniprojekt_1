eadd113d51b945f6982ca5d15db6d3b43c1e86dd
Hanokos <119046964+Hanokos@users.noreply.github.com>
2023-02-23 11:29:13 +0100

    Update Determinant.m

--- Determinant.m
+++ Determinant.m
@@ -2,6 +2,7 @@
 clear
 close all
 
+%% Matriser
 % Definiera rektangelns sidor som kolumner i matrisen A
 A = [1 0; 
      0 1];
@@ -10,6 +11,7 @@ A = [1 0;
 B =  [2 0; 
       0 1.4];
 
+%% Ekvationer
 % Transformera rektangeln genom att multiplicera A med B
 % Beskriver hur den ursprungliga rektangeln har förändrats 
 AB = A*B;
@@ -17,7 +19,7 @@ AB = A*B;
 % Beräkna determinantvärdet för AB
 det_AB = det(AB);
 
-
+%% Definera formerna
 % Definiera hörnen för rektangeln som en matris med koordinaterna för hörnen som rader
 fyrkantA = [0 0; A(1,1) 0; A(1,1) A(2,2); 0 A(2,2); 0 0];
 
@@ -25,7 +27,8 @@ fyrkantA = [0 0; A(1,1) 0; A(1,1) A(2,2); 0 A(2,2); 0 0];
 % Detta för vi vet med AB hur den förändras men inte dens lokation, vilket vi får via fyrkantA.
 rektangelB = fyrkantA * AB;
 
-% Rita fyrkantA och parallelogrammen i en figur
+
+%% Rita fyrkantA och rektangelB i en figur
 figure;
 hold on;
 plot(fyrkantA(:,1), fyrkantA(:,2), 'r'); % Jag tar kolumn 1 för x och kolumn 2 för y värden0
