ad4699aee2e54a7d6ab8b7562dbdd28379bc9b40
Hanokos <119046964+Hanokos@users.noreply.github.com>
2023-02-23 11:29:54 +0100

    Update miniprojekt1.m

--- miniprojekt1.m
+++ miniprojekt1.m
@@ -1,57 +1,67 @@
 clear
 close all
 
-startmatris=[0 2 2 7 7 2 2 8 8 0 0; 0 0 8 8 10 10 14 14 16 16 0];
-xpos = startmatris(1,:);
-ypos = startmatris(2,:);
+%% Startmatris
+startmatris=[0 2 2 4 4 6 6 8 8 6 6 10 10 6 6 4 4 2 2 0 0 ; % Ser ut som ett H med ett F i sig
+             0 0 4 4 -2 -2 0 0 2 2 4 4 6 6 10 10 6 6 10 10 0 ];
 
-% scale
-xscale = 2*xpos;
+xpos = startmatris(1,:); %Rad 1 = x värden
+ypos = startmatris(2,:); %Rad 2 = y värden
+%% Omskalning
+xscale = 2*xpos; 
 yscale = 2*ypos;
+
+A = polyarea(xscale,yscale); % Räknar arean
+B = polyarea(xpos,ypos);
 
 %plot scale
 figure(1)
 plot(xpos, ypos,'-b',xscale,yscale,'-r')
 axis equal;
+title('Omskalning');
+subtitle(['\color{blue}Area innan = ', num2str(B),'  \color{red}Area efter = ', num2str(A)])
 legend('innan', 'efter')
 
-
-%Rotation
-my_angle = 90; %angle in degrees
-xr = cosd(my_angle)*xpos - sind(my_angle)*ypos ;
-yr = cosd(my_angle)*ypos + sind(my_angle)*xpos ;
+%% Rotation
+%prompt = "Lägg in rotering (i grader): "; %Om man vill ha input
+%theta = input(prompt);
+theta = 90; %Vinkel i grader (pi/2)
+xr = cosd(theta)*xpos - sind(theta)*ypos ;
+yr = cosd(theta)*ypos + sind(theta)*xpos ;
 
 %plot rotation
 figure(2)
-plot(xpos,ypos,'-b',xr,yr)
+plot(xpos,ypos,'-b',xr,yr,'-r')
 axis equal;
-legend('innan', 'efter')
+title('Rotation')
+subtitle([' \color{red}Roterad ', num2str(theta),' Grader'])
+legend('Innan', 'Efter')
 
-%plot skjuvning
+%% Skjuvning
 figure(3)
 plot(xpos,ypos,'-b',xpos+ ypos*-0.7,ypos,'-r')
 axis equal;
-legend('innan', 'efter')
+title('Skjuvning')
+legend('Innan', 'Efter')
 
-%projektion
-xp = cosd(my_angle)^2*xpos + sind(my_angle)*cosd(my_angle)*ypos ;
-yp = sind(my_angle)*cosd(my_angle)*ypos + sind(my_angle)^2*xpos ;
+%% Projektion
+xp = cosd(theta)^2*xpos + sind(theta)*cosd(theta)*ypos ;
+yp = sind(theta)*cosd(theta)*ypos + sind(theta)^2*xpos ;
 
-%plot
+%plot projektion
 figure(4)
-plot(xpos,ypos,'-r',xp,yp,'-b')
+plot(xpos,ypos,'-b',xp,yp,'-r')
 axis equal;
-legend('innan', 'efter')
+title('Projektion')
+legend('Innan', 'Efter')
 
-%Spegling
-xs = cosd(2 *my_angle)*xpos + sind(2 *my_angle)*ypos ;
-ys = -cosd(2 * my_angle) * ypos + sind(2 *my_angle)*xpos ;
+%% Spegling
+xs = cosd(2 *theta)*xpos + sind(2 *theta)*ypos ;
+ys = -cosd(2 * theta) * ypos + sind(2 *theta)*xpos ;
 
 %plot spegling
 figure(5)
-plot(xpos,ypos,'-r',xs,ys,'-b')
+plot(xpos,ypos,'-b',xs,ys,'-r')
 axis equal;
-legend('innan', 'efter')
-
-
-
+title('Spegling')
+legend('Innan', 'Efter')
