23a6320c017bf8ab37de06724dd0648ad957a717
Hanokos <119046964+Hanokos@users.noreply.github.com>
2023-02-28 14:45:03 +0100

    Update Determinant.m

--- Determinant.m
+++ Determinant.m
@@ -1,4 +1,4 @@
-%Determinant
+%% Determinat genom Skjuvning
 clear
 close all
 
@@ -10,7 +10,6 @@ startmatris=[0 2 2 4 4 6 6 8 8 6 6 10 10 6 6 4 4 2 2 0 0 ; % Ser ut som ett H me
 xpos = startmatris(1,:); %Rad 1 = x värden
 ypos = startmatris(2,:); %Rad 2 = y värden
 
-%% Determinat genom Skjuvning
 % Definiera hur mycket matrisen ska skjutas i x-led
 a = -0.6;
 
@@ -23,8 +22,7 @@ area_startmatris = abs(polyarea(xpos,ypos));
 
 % Area av avbildningsmatrisen
 area_avbildning = abs(polyarea(xs,ypos));
-
-%Plot startmatrisen och avbidlingmatrisen
+%% Plot startmatrisen och avbildningsmatrisen
 figure(1)
 plot(xpos,ypos,'-b',xs,ypos,'-r')
 axis equal;
