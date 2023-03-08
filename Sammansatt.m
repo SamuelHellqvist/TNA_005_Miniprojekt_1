32bd5436383d86255cb48b5f9c76a084e57ef617
SamuelHellqvist <85281591+SamuelHellqvist@users.noreply.github.com>
2023-03-08 09:21:16 +0100

    2023-03-08
    lite kod när två sammansatta avbildningar finns med

--- Sammansatt.m
+++ Sammansatt.m
@@ -25,3 +25,21 @@ figure(1)
 plot(xpos,ypos,'-b',xs,yr,'-r')
 axis equal;
 legend('innan', 'efter')
+
+%%Figur som skjuvas och roteras
+%skjuva
+a = -0.6;
+
+xs = xpos + ypos * a;
+
+%rotation
+theta = 30; %Vinkel i grader
+xr = cosd(theta)*xs - sind(theta)*ypos ;
+yr = cosd(theta)*ypos + sind(theta)*xs ;
+
+figure(2)
+plot(xpos,ypos,'-b',xr,yr,'-r', LineWidth=2)
+axis equal;
+grid on;
+legend('innan', 'efter')
+title('Figur som har skjuvats och sedan roteras', fontSize=15)
